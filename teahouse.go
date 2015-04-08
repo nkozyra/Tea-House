818f84a10cc9f0b5d00eea4f3052a9833069cd37
Nathan Kozyra <nkozyra@gmail.com>
2015-04-07 23:05:50 -0400

    Basic template scaffolding

--- teahouse.go
+++ teahouse.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
 )
 
+const (
+	PORT = ":8080"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+type Page struct {
+	Title string
+	Body  string
+}
+
+func loadPage(title string) Page {
+	return Page{Title: title, Body: ""}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	p := loadPage("login")
+	// t, _ := template.ParseFiles("templates/login.html")
+	templates.Execute(w, p)
+}
+
 func main() {
-	log.Println("Starting Tea House")
+	fmt.Println("Starting")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", LoginHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(PORT, nil)
 }
